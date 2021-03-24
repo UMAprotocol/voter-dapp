6246a91d4e7b8f073c4d8d2156c3816d30795348
Jason Kiraly <jasonkiraly@Jasons-MBP.hitronhub.home>
2021-03-24 10:46:24 -0700

    fixed issues with tailwind integration.

--- src/features/vote/Vote.tsx
+++ src/features/vote/Vote.tsx
@@ -1,5 +1,13 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from "twin.macro";
+
 const Vote = () => {
-  return <div>Vote Page</div>;
+  return (
+    <div tw="max-w-4xl mx-auto p-5 mt-5">
+      <h1 tw="text-blue-500 text-4xl">Vote Page</h1>
+    </div>
+  );
 };
 
 export default Vote;
