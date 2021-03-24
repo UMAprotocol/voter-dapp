4e20bcf14421c51f2ee7fb36aa0426bed0e63d69
Jason Kiraly <jasonkiraly@Jasons-MBP.hitronhub.home>
2021-03-24 15:16:34 -0700

    stubbing navbar.

--- src/common/components/navbar/Navbar.tsx
+++ src/common/components/navbar/Navbar.tsx
@@ -1,10 +1,96 @@
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line
-import tw from "twin.macro";
+import tw, { styled } from "twin.macro";
 
-// const Wrapper =
+import { Link } from "react-router-dom";
+import logo from "assets/logo.png";
+import discordLogo from "assets/ico-discord.svg";
+import githubLogo from "assets/ico-github.svg";
+import mediumLogo from "assets/ico-medium.svg";
+import twitterLogo from "assets/ico-tw.svg";
+
+const Wrapper = styled.nav`
+  margin: 0 auto;
+  margin-top: 1rem;
+  .logo {
+    height: 30px;
+  }
+  .sm-logo {
+    height: 25px;
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+  .link {
+    &:hover {
+      color: red;
+    }
+  }
+`;
+
 const Navbar = () => {
-  return <div />;
+  return (
+    <Wrapper tw="flex justify-between flex-wrap bg-white p-3 max-w-7xl">
+      <Link tw="inline-flex items-center p-2 mr-4" to="/">
+        <img className="logo" src={logo} alt="uma_logo" />
+      </Link>
+      <div tw="inline-flex items-center">
+        <Link to="/" tw=""></Link>
+        <div tw="flex place-items-end">
+          <Link className="link" to="/" tw="px-5 py-3">
+            Vote
+          </Link>
+          <Link className="link" to="/" tw="px-5 py-3">
+            FAQs
+          </Link>
+          <a
+            className="link"
+            href="https://docs.umaproject.org/"
+            target="_blank"
+            tw="px-5 py-3"
+            rel="noreferrer"
+          >
+            Docs
+          </a>
+          <Link className="link" to="/" tw="px-5 py-3">
+            About
+          </Link>
+          <a
+            href="https://medium.com/uma-project"
+            tw="px-5 py-3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="sm-logo" src={mediumLogo} alt="medium_logo" />
+          </a>
+          <a
+            href="https://github.com/UMAprotocol"
+            tw="px-5 py-3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="sm-logo" src={githubLogo} alt="medium_logo" />
+          </a>
+          <a
+            href="https://twitter.com/UMAprotocol"
+            tw="px-5 py-3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="sm-logo" src={twitterLogo} alt="medium_logo" />
+          </a>
+          <a
+            href="https://medium.com"
+            tw="px-5 py-3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="sm-logo" src={discordLogo} alt="medium_logo" />
+          </a>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Navbar;
--- src/common/router/Router.tsx
+++ src/common/router/Router.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+// Pages
 import Vote from "features/vote";
+
+// Components
+import Navbar from "common/components/navbar";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <div>
+        <Navbar />
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
