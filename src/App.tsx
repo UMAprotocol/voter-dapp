378013c69860bf81242460346023d199b6ada167
Jason Kiraly <jaykiraly@gmail.com>
2021-03-25 13:05:54 -0700

    feat: scaffolded code (#1)
    * Initialize project using Create React App
    
    * Router and folder.
    
    * feat: scaffolding of app complete with base dependencies.
    
    * Initialize project using Create React App
    
    * Router and folder.
    
    * feat: scaffolding of app complete with base dependencies.
    
    * fixed issues with tailwind integration.
    
    * removed unnec dependency
    
    Co-authored-by: Jason Kiraly <jasonkiraly@Jasons-MBP.hitronhub.home>

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Router from "common/router";
+
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/vote/Vote.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from "twin.macro";
+
+const Vote = () => {
+  return (
+    <div tw="max-w-4xl mx-auto p-5 mt-5">
+      <h1 tw="text-blue-500 text-4xl">Vote Page</h1>
+    </div>
+  );
+};
+
+export default Vote;
