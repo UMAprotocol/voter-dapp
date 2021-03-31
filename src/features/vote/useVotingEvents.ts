c26cb54af0db39a486597095f69b0dce16a1f787
Tulun <jaykiraly@gmail.com>
2021-03-31 10:39:59 -0700

    slight reorg of file.

--- src/features/vote/useVotingEvents.ts
+++ src/features/vote/useVotingEvents.ts
@@ -8,6 +8,18 @@ export interface PriceRound {
   time: string;
 }
 
+export default function usePriceRounds(contract: ethers.Contract | null) {
+  const [priceRounds, setPriceRounds] = useState<PriceRound[]>([]);
+
+  useEffect(() => {
+    if (contract) {
+      queryPriceRoundEvents(contract, setPriceRounds);
+    }
+  }, [contract]);
+
+  return { priceRounds };
+}
+
 const queryPriceRoundEvents = async (
   contract: ethers.Contract,
   setState: Function
@@ -20,36 +32,24 @@ const queryPriceRoundEvents = async (
     null
   );
 
-  const events = await contract.queryFilter(
-    priceRequestFilter,
-    MAINNET_DEPLOY_BLOCK
-  );
+  try {
+    const events = await contract.queryFilter(
+      priceRequestFilter,
+      MAINNET_DEPLOY_BLOCK
+    );
 
-  const pr = events.map((el) => {
-    const { args } = el;
-    const datum = {} as PriceRound;
-    if (args) {
-      datum.roundId = args[0].toString();
-      datum.identifier = ethers.utils.toUtf8String(args[1]);
-      datum.time = args[2].toString();
-    }
-    return datum;
-  });
-  setState(pr);
-};
-
-export default function usePriceRounds(contract: ethers.Contract | null) {
-  const [priceRounds, setPriceRounds] = useState<PriceRound[]>([]);
-  useEffect(() => {
-    if (contract) {
-      queryPriceRoundEvents(contract, setPriceRounds);
-
-      try {
-      } catch (err) {
-        console.log("err", err);
+    const pr = events.map((el) => {
+      const { args } = el;
+      const datum = {} as PriceRound;
+      if (args) {
+        datum.roundId = args[0].toString();
+        datum.identifier = ethers.utils.toUtf8String(args[1]);
+        datum.time = args[2].toString();
       }
-    }
-  }, [contract]);
-
-  return { priceRounds };
-}
+      return datum;
+    });
+    setState(pr);
+  } catch (err) {
+    console.log("err", err);
+  }
+};
