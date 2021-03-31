54ef96cd4a3d599d633632585321c5ddb2399607
Tulun <jaykiraly@gmail.com>
2021-03-31 14:54:33 -0700

    fixed string format func.

--- src/features/vote/Wallet.tsx
+++ src/features/vote/Wallet.tsx
@@ -13,7 +13,7 @@ interface Props {
   // disconnect: Disconnect;
 }
 
-const DEFAULT_BALANCE = ["0.000", "0000"];
+const DEFAULT_BALANCE = ["0.", "0000000"];
 
 const Wallet: FC<Props> = () => {
   const [umaBalance, setUmaBalance] = useState(DEFAULT_BALANCE);
@@ -212,17 +212,30 @@ const Disconnected = styled(Connected)`
 
 // There are two different colours for the first 4 digits and last 4 digits of the number.
 // Hence we need to return an array of strings.
+// We want to limit the overall string length to ~8 digits.
 function formatWalletBalance(balance: string): string[] {
   if (balance.includes(".")) {
     const strArray: string[] = [];
     const split = balance.split(".");
-    const rightSide = split[1].substr(0, 8);
-
-    strArray.push(`${split[0]}.${rightSide.substr(0, 3)}`);
-    strArray.push(rightSide.substr(4, 8));
+    const MAX_LENGTH_RIGHT_SIDE = 9 - split[0].length;
+    let rightSide = "";
+    // Need to confirm is the left whole number isn't a massive amount.
+    // If it is, just show 1 decimal.
+    if (MAX_LENGTH_RIGHT_SIDE > 0) {
+      rightSide = split[1].substr(0, MAX_LENGTH_RIGHT_SIDE);
+    } else {
+      rightSide = split[1].substr(0, 1);
+    }
+    strArray.push(`${split[0]}.`);
+    strArray.push(rightSide.substr(0, 8));
     return strArray;
   } else {
-    return [`${balance}.000`, "0000"];
+    const MAX_LENGTH_RIGHT_SIDE = 9 - balance.length;
+    let trailingZeros = "";
+    for (let i = 0; i < MAX_LENGTH_RIGHT_SIDE; i++) {
+      trailingZeros = trailingZeros.concat("0");
+    }
+    return [`${balance}.`, trailingZeros];
   }
 }
 
