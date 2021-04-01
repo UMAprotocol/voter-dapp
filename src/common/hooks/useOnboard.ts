7acd012abc03194e9849711ea32a56f7c591fced
Tulun <jaykiraly@gmail.com>
2021-04-01 12:03:21 -0700

    renamed to useOnboard.

---
+++ src/common/hooks/useOnboard.ts
@@ -0,0 +1,77 @@
+import { useContext, useEffect, useState } from "react";
+import { OnboardContext, actions } from "common/context/OnboardContext";
+import { ethers } from "ethers";
+import { Wallet } from "bnc-onboard/dist/src/interfaces";
+import { SUPPORTED_NETWORK_IDS } from "common/config";
+
+export default function useOnboard() {
+  const [initOnboard, setInitOnboard] = useState(false);
+  const context = useContext(OnboardContext);
+  if (!Object.keys(context).length) {
+    throw new Error(`UseConnection must be used within a Connection Provider.`);
+  }
+
+  const {
+    state: { provider, onboard, signer, network, address, error, isConnected },
+    dispatch,
+    connect,
+    disconnect,
+  } = context;
+
+  // When network changes, reconnect
+  useEffect(() => {
+    if (initOnboard) {
+      // These are optional callbacks to be passed into onboard.
+      const subscriptions = {
+        address: (address: string | null) => {
+          console.log("does this run on address change in MM?", address);
+          dispatch({ type: actions.SET_ADDRESS, payload: address });
+        },
+        network: async (networkId: any) => {
+          if (!SUPPORTED_NETWORK_IDS.includes(networkId) && networkId != null) {
+            throw new Error(
+              "This dApp will work only with the Mainnet or Kovan network"
+            );
+          }
+          onboard?.config({ networkId: networkId });
+        },
+        wallet: async (wallet: Wallet) => {
+          if (wallet.provider) {
+            const ethersProvider = new ethers.providers.Web3Provider(
+              wallet.provider
+            );
+            dispatch({ type: actions.SET_PROVIDER, payload: ethersProvider });
+            dispatch({
+              type: actions.SET_SIGNER,
+              payload: ethersProvider.getSigner(),
+            });
+            dispatch({
+              type: actions.SET_NETWORK,
+              payload: await ethersProvider.getNetwork(),
+            });
+          } else {
+            dispatch({ type: actions.SET_PROVIDER, payload: null });
+            dispatch({ type: actions.SET_NETWORK, payload: null });
+          }
+        },
+      };
+
+      connect(dispatch, network, subscriptions);
+      setInitOnboard(false);
+    }
+  }, [network, connect, initOnboard, setInitOnboard, dispatch, onboard]);
+
+  return {
+    provider,
+    onboard,
+    signer,
+    network,
+    address,
+    error,
+    isConnected,
+    connect,
+    disconnect: () => disconnect(dispatch, onboard),
+    initOnboard,
+    setInitOnboard,
+  };
+}
