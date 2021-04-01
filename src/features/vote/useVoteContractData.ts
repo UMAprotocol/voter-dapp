52a0b412944aa03f2bebc5d57fc2e13f30e00b7e
Tulun <jaykiraly@gmail.com>
2021-04-01 12:15:53 -0700

    all events added.

--- src/features/vote/useVoteContractData.ts
+++ src/features/vote/useVoteContractData.ts
@@ -10,10 +10,15 @@ export default function useVoteContractData(
   const [votesCommitted, setVotesCommitted] = useState<VoteEvent[]>([]);
   const [encryptedVotes, setEncryptedVotes] = useState<VoteEvent[]>([]);
   const [votesRevealed, setVotesRevealed] = useState<VoteRevealed[]>([]);
+  const [rewardsRetrieved, setRewardsRetrieved] = useState<RewardsRetrieved[]>(
+    []
+  );
+
+  const [priceResolved, setPriceResolved] = useState<PriceResolved[]>([]);
 
   useEffect(() => {
     if (contract && address) {
-      console.log("cntract", contract);
+      console.log("<<<<contract", contract);
       queryVotesCommitted(contract, address).then((data) => {
         if (data) setVotesCommitted(data);
       });
@@ -21,13 +26,33 @@ export default function useVoteContractData(
         if (data) setEncryptedVotes(data);
       });
       queryVoteRevealed(contract, address).then((data) => {
-        console.log("revealed data", data);
         if (data) setVotesRevealed(data);
       });
+      queryRewardsRetrieved(contract, address).then((data) => {
+        console.log("query rewards", data);
+        if (data) setRewardsRetrieved(data);
+      });
+      queryPriceResolved(contract).then((data) => {
+        console.log("price resolved", data);
+        if (data) setPriceResolved(data);
+      });
+    } else {
+      // Reset values if they disconnect.
+      setVotesCommitted([]);
+      setEncryptedVotes([]);
+      setVotesRevealed([]);
+      setRewardsRetrieved([]);
+      setPriceResolved([]);
     }
   }, [contract, address]);
 
-  return { votesCommitted, encryptedVotes, votesRevealed };
+  return {
+    votesCommitted,
+    encryptedVotes,
+    votesRevealed,
+    rewardsRetrieved,
+    priceResolved,
+  };
 }
 
 /*  event VoteCommitted(
@@ -105,11 +130,8 @@ const queryEncryptedVotes = async (
   );
   try {
     const events = await contract.queryFilter(filter, MAINNET_DEPLOY_BLOCK);
-    const filteredEventsByAddress = events.filter(
-      (el) => el.args && el.args[0].toLowerCase() === address.toLowerCase()
-    );
 
-    return filteredEventsByAddress.map((el) => {
+    return events.map((el) => {
       const { args } = el;
       const datum = {} as VoteEvent;
       if (args) {
@@ -175,6 +197,103 @@ const queryVoteRevealed = async (
         datum.time = args[3].toString();
         datum.price = args[4].toString();
         datum.numTokens = args[6].toString();
+      }
+
+      return datum;
+    });
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+
+/*
+  event RewardsRetrieved(
+    address indexed voter,
+    uint256 indexed roundId,
+    bytes32 indexed identifier,
+    uint256 time,
+    bytes ancillaryData,
+    uint256 numTokens
+  );
+*/
+
+interface RewardsRetrieved extends VoteEvent {
+  numTokens: string;
+}
+
+const queryRewardsRetrieved = async (
+  contract: ethers.Contract,
+  address: string
+) => {
+  // BIG NOTE. You need to pass in null for events with args.
+  // Otherwise this will likely return no values.
+  // RewardsRetrieved(address,uint256,bytes32,uint256,bytes,uint256)
+  const filter = contract.filters.RewardsRetrieved(
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
+  );
+
+  try {
+    const events = await contract.queryFilter(filter, MAINNET_DEPLOY_BLOCK);
+    const filteredEventsByAddress = events.filter(
+      (el) => el.args && el.args[0].toLowerCase() === address.toLowerCase()
+    );
+
+    return filteredEventsByAddress.map((el) => {
+      const { args } = el;
+      const datum = {} as RewardsRetrieved;
+      if (args) {
+        datum.address = args[0];
+        datum.roundId = args[1].toString();
+        datum.identifier = ethers.utils.toUtf8String(args[2]);
+        datum.time = args[3].toString();
+        datum.numTokens = args[5].toString();
+      }
+
+      return datum;
+    });
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+/*
+event PriceResolved(
+  uint256 indexed roundId,
+  bytes32 indexed identifier,
+  uint256 time,
+  int256 price,
+  bytes ancillaryData
+);
+*/
+
+interface PriceResolved {
+  roundId: string;
+  identifier: string;
+  time: string;
+  price: string;
+}
+
+const queryPriceResolved = async (contract: ethers.Contract) => {
+  // BIG NOTE. You need to pass in null for events with args.
+  // Otherwise this will likely return no values.
+  // PriceResolved(uint256,bytes32,uint256,int256,bytes)
+  const filter = contract.filters.PriceResolved(null, null, null, null, null);
+
+  try {
+    const events = await contract.queryFilter(filter, MAINNET_DEPLOY_BLOCK);
+
+    return events.map((el) => {
+      const { args } = el;
+      const datum = {} as PriceResolved;
+      if (args) {
+        datum.roundId = args[0].toString();
+        datum.identifier = ethers.utils.toUtf8String(args[1]);
+        datum.time = args[2].toString();
+        datum.price = args[3].toString();
       }
 
       return datum;
