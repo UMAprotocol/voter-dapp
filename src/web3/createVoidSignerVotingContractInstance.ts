9087e921a64c1bc4b8caaf90b7643d83e8be49e5
Tulun <jaykiraly@gmail.com>
2021-04-09 09:39:59 -0700

    updated documentation on void signer.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/web3/createVoidSignerVotingContractInstance.ts
+++ src/web3/createVoidSignerVotingContractInstance.ts
@@ -1,6 +1,6 @@
 import VotingArtifact from "@uma/core/build/contracts/Voting.json";
 import { ethers } from "ethers";
-console.log("VotingArt", VotingArtifact);
+
 interface Network {
   [key: string]: {
     address: string;
@@ -10,6 +10,9 @@ interface Network {
   };
 }
 
+// We need to query the data before a user has logged in
+// Because the provider doesn't have a default signer, we provide one with Void Signer, which creates a read only signer.
+// See: https://docs.ethers.io/v5/api/signer/#VoidSigner
 export default function createVoidSignerVotingContractInstance(
   provider: ethers.providers.JsonRpcProvider,
   networkId: string
