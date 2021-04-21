9f22dbba89a16a602b170c3530f2870abbdf3f60
Tulun <jaykiraly@gmail.com>
2021-04-21 14:12:14 -0700

    improved calc uma balance func.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/features/wallet/helpers/calculateUMATotalValue.ts
+++ src/features/wallet/helpers/calculateUMATotalValue.ts
@@ -1,9 +1,11 @@
+import { ethers } from "ethers";
+
+const ZERO_BALANCE = "0.0";
+
 export default function calculateUMATotalValue(price: number, balance: string) {
-  return (
-    (price * Number(balance))
-      .toFixed(2)
-      // Add commas
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-      .toLocaleString()
+  const formattedString = ethers.utils.commify(
+    (price * Number(balance)).toFixed(2).toLocaleString()
   );
+  if (formattedString !== ZERO_BALANCE) return formattedString;
+  return "0.00";
 }
