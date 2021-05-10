f4c65c3bdde899b4d167baeb97c3ef64fe744ca1
Tulun <jaykiraly@gmail.com>
2021-05-10 16:24:21 -0700

    commify balances.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/features/wallet/helpers/formatWalletBalance.ts
+++ src/features/wallet/helpers/formatWalletBalance.ts
@@ -1,6 +1,7 @@
 // There are two different colours for the first 4 digits and last 4 digits of the number.
 // Hence we need to return an array of strings.
 // We want to limit the overall string length to ~8 digits.
+import { ethers } from "ethers";
 export default function formatWalletBalance(balance: string): string[] {
   if (balance.includes(".")) {
     const strArray: string[] = [];
@@ -22,7 +23,7 @@ export default function formatWalletBalance(balance: string): string[] {
     } else {
       rightSide = split[1].substr(0, 1);
     }
-    strArray.push(`${split[0]}.`);
+    strArray.push(`${ethers.utils.commify(split[0])}.`);
     strArray.push(rightSide.substr(0, 8));
     return strArray;
   } else {
@@ -31,6 +32,6 @@ export default function formatWalletBalance(balance: string): string[] {
     for (let i = 0; i < MAX_LENGTH_RIGHT_SIDE; i++) {
       trailingZeros = trailingZeros.concat("0");
     }
-    return [`${balance}.`, trailingZeros];
+    return [`${ethers.utils.commify(balance)}.`, trailingZeros];
   }
 }
