c9e81ac1075ea98d1517208eb7e8e59e21cb0ba9
Tulun <jaykiraly@gmail.com>
2021-05-17 12:25:41 -0700

    add h and s to time.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/features/vote/helpers/calculateTimeRemaining.ts
+++ src/features/vote/helpers/calculateTimeRemaining.ts
@@ -8,6 +8,12 @@ export const calculateTimeRemaining = () => {
   let text = "00:00";
   // format difference
   if (difference > 0) text = Duration.fromMillis(difference).toFormat("hh:mm");
+  // add h and m to time.
+  const split = text.split(":");
+  const ls = `${split[0]}h`;
+  const rs = `${split[1]}m`;
+
+  text = `${ls}:${rs}`;
 
   return text;
 };
