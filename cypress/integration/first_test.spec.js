0c541e2bc85641802e40bb52a0e80808af65268a
Tulun <jaykiraly@gmail.com>
2021-06-01 13:34:27 -0700

    wip.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- cypress/integration/first_test.spec.js
+++ cypress/integration/first_test.spec.js
@@ -3,8 +3,18 @@ describe("My First Test", () => {
     cy.visit("localhost:3000");
 
     cy.contains("Voting Wallet");
+    // cy.visit(
+    //   "chrome-extension://hgoafkmacacpcipakokjkkpodaonghel/home.html#initialize/welcome"
+    //   // "https://google.com"
+    // );
 
-    // cy.get(".connect-btn").click();
-    // cy.get(".svelte-1skxsnk").first().click();
+    cy.get(".connect-btn").click();
+
+    cy.get(".svelte-1skxsnk").first().click();
+    cy.get(
+      ".bn-onboard-custom.bn-onboard-prepare-button.svelte-r5g1v4.bn-onboard-prepare-button-center"
+    )
+      .first()
+      .click();
   });
 });
