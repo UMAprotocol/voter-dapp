e861b5226da7e3cffe4ef28ace27fbd5be228b07
Tulun <jaykiraly@gmail.com>
2021-06-01 14:31:48 -0700

    hold off for now,
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- cypress/integration/first_test.spec.js
+++ cypress/integration/first_test.spec.js
@@ -1,20 +1,28 @@
+const { createYield } = require("typescript");
+
 describe("My First Test", () => {
-  it("Visits localhost", () => {
-    cy.visit("localhost:3000");
-
-    cy.contains("Voting Wallet");
-    // cy.visit(
-    //   "chrome-extension://hgoafkmacacpcipakokjkkpodaonghel/home.html#initialize/welcome"
-    //   // "https://google.com"
-    // );
-
-    cy.get(".connect-btn").click();
-
-    cy.get(".svelte-1skxsnk").first().click();
-    cy.get(
-      ".bn-onboard-custom.bn-onboard-prepare-button.svelte-r5g1v4.bn-onboard-prepare-button-center"
-    )
-      .first()
-      .click();
-  });
+  // it("downloads metamask", () => {
+  //   cy.visit("https://metamask.io/download");
+  //   cy.get(".download-block").first().click();
+  //   cy.wait(2000);
+  // });
+  // it("Visits localhost", () => {
+  //   cy.visit("localhost:3000");
+  //   cy.contains("Voting Wallet");
+  //   cy.get(".connect-btn").click();
+  //   cy.get(".svelte-1skxsnk").first().click();
+  //   cy.get(
+  //     ".bn-onboard-custom.bn-onboard-prepare-button.svelte-r5g1v4.bn-onboard-prepare-button-center"
+  //   )
+  //     .first()
+  //     .click();
+  //   cy.reload();
+  //   cy.get(".connect-btn").click();
+  //   cy.get(".svelte-1skxsnk").first().click();
+  //   // cy.get(
+  //   //   ".bn-onboard-custom.bn-onboard-prepare-button.svelte-r5g1v4.bn-onboard-prepare-button-center"
+  //   // )
+  //   //   .first()
+  //   //   .click();
+  // });
 });
