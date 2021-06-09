98ff08e0a081239d74a8fbf8cfccb6a842967bb0
Tulun <jaykiraly@gmail.com>
2021-06-09 14:23:45 -0700

    updated thegraph url.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/common/apollo/client.ts
+++ src/common/apollo/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/umaprotocol/mainnet-voting",
+  uri: `https://gateway.thegraph.com/api/${process.env.REACT_APP_THE_GRAPH_API_KEY}/subgraphs/id/0x0a0319671f2d3c18fb55ab555b48bc01f27747a4-0`,
   cache: new InMemoryCache(),
 });
