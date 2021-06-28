37a0e408a3417fe91749fdb57d15ffc3ccb40577
Tulun <jaykiraly@gmail.com>
2021-06-28 11:39:52 -0700

    remove commented code.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/common/helpers/getSigningKeys.ts
+++ src/common/helpers/getSigningKeys.ts
@@ -1,37 +1,5 @@
 import { SigningKeys } from "App";
 
-// export default function getSigningKeys(
-//   signingKeys: SigningKeys,
-//   hexMessage: string,
-//   votingAddress: string | null,
-//   hotAddress: string | null
-// ) {
-//   const keys = {
-//     publicKey: "",
-//     privateKey: "",
-//   };
-
-//   if (hexMessage && votingAddress && Object.keys(signingKeys).length) {
-//     if (hotAddress) {
-//       keys.privateKey = signingKeys[hexMessage][hotAddress]
-//         ? signingKeys[hexMessage][hotAddress].privateKey
-//         : "";
-//       keys.publicKey = signingKeys[hexMessage][hotAddress]
-//         ? signingKeys[hexMessage][hotAddress].publicKey
-//         : "";
-//     } else {
-//       keys.privateKey = signingKeys[hexMessage][votingAddress]
-//         ? signingKeys[hexMessage][votingAddress].privateKey
-//         : "";
-//       keys.publicKey = signingKeys[hexMessage][votingAddress]
-//         ? signingKeys[hexMessage][votingAddress].publicKey
-//         : "";
-//     }
-//   }
-
-//   return keys;
-// }
-
 export default function getSigningKeys(
   signingKeys: SigningKeys,
   address: string | null
