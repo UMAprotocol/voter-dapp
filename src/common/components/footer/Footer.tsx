790e79aac8bca668ea500091d3c301fee0817088
Tulun <jaykiraly@gmail.com>
2021-07-14 12:00:46 -0700

    Footer.styled
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 import tw, { styled } from "twin.macro"; // eslint-disable-line
 import { FooterLogo } from "assets/icons";
 import { Discord, Github, Medium, Twitter } from "assets/icons";
+import { StyledFooter } from "./Footer.styled";
 
 const Footer: FC = () => {
   return (
@@ -98,82 +99,5 @@ const socialLinks = [
     logo: <Discord className="sm-logo" />,
   },
 ];
-
-const StyledFooter = styled.div`
-  max-width: 1280px;
-  margin: 3rem auto 0;
-  font-family: "Halyard Display";
-  min-height: 20vh;
-  .logo-wrapper {
-    flex: 0 0 24%;
-  }
-  .link-wrapper {
-    padding: 0 100px;
-    flex: 1 1;
-    border-left: 1px solid rgba(0, 0, 0, 0.1);
-    border-right: 1px solid rgba(0, 0, 0, 0.1);
-  }
-  .links {
-    display: flex;
-    flex-wrap: wrap;
-    .link {
-      flex-basis: 50%;
-      margin-bottom: 1.25rem;
-      font-weight: 600;
-      a {
-        &:hover {
-          text-decoration: underline;
-        }
-      }
-    }
-  }
-  .subscribe-wrapper {
-    flex: 0 0 32%;
-    max-width: 32%;
-    padding-left: 30px;
-    text-align: right;
-    .subscribe {
-      display: inline-block;
-      max-width: 285px;
-      width: 100%;
-      margin-bottom: 33px;
-      padding: 5px 0 0;
-      text-align: left;
-      .header {
-        text-align: left;
-        color: #ff4a4a;
-        margin-bottom: 0.8rem;
-      }
-      .sub-text {
-        text-align: left;
-        font-size: 0.8rem;
-        opacity: 0.75;
-        margin: 0 0 15px;
-        color: #000;
-        line-height: 1.57;
-      }
-    }
-    .sm-links-wrapper {
-      display: flex;
-      max-width: 50%;
-    }
-    .logo {
-      height: 30px;
-    }
-    .sm-logo {
-      height: 25px;
-      &:hover {
-        transition: opacity 0.2s;
-        opacity: 0.7;
-      }
-    }
-    .link {
-      &:hover {
-        transition: color 0.3s;
-        color: #ff4a4a;
-      }
-    }
-  }
-`;
 
 export default Footer;
