9bd51a19f033db76e0699b56c1918e91bda6e47d
Jason Kiraly <jaykiraly@gmail.com>
2021-10-12 06:58:45 -0700

    feat: Sign only once a round. (#67)
    * useSigningKeys hook.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * set signing keys in localStorage.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * keys set in local storage.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * updated type and comment.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * added wallet check back in.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * instantiate localStorage get in useState call.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ src/hooks/useSigningKeys.ts
@@ -0,0 +1,86 @@
+import { ethers } from "ethers";
+import { useState, useEffect, useContext } from "react";
+import { ErrorContext } from "common/context/ErrorContext";
+
+import { recoverPublicKey } from "features/vote/helpers/recoverPublicKey";
+import { derivePrivateKey } from "features/vote/helpers/derivePrivateKey";
+import usePrevious from "common/hooks/usePrevious";
+
+// User has to sign once per round.
+// public and private signing keys is indexed by address, and we ask them to resign if the roundMessage changes.
+export interface SigningKeys {
+  [address: string]: {
+    publicKey: string;
+    privateKey: string;
+    roundMessage: string;
+  };
+}
+
+export default function useSigningKeys(
+  signer: ethers.Signer | null,
+  address: string | null,
+  roundId: string
+) {
+  const [signingKeys, setSigningKeys] = useState<SigningKeys>(() => {
+    const storageKeys = localStorage.getItem("signingKeys");
+    return storageKeys ? (JSON.parse(storageKeys) as SigningKeys) : {};
+  });
+
+  const { addError } = useContext(ErrorContext);
+
+  const previousSigner = usePrevious(signer);
+  const previousAddress = usePrevious(address);
+
+  useEffect(() => {
+    if (
+      signer &&
+      address &&
+      roundId &&
+      (previousAddress === null || previousSigner === null)
+    ) {
+      const message = `UMA Protocol one time key for round: ${roundId}`;
+      const keyExists = signingKeys[address];
+      if (!keyExists || keyExists.roundMessage !== message) {
+        signer
+          .signMessage(message)
+          .then((msg) => {
+            const key = {} as {
+              publicKey: string;
+              privateKey: string;
+              roundMessage: string;
+            };
+
+            const privateKey = derivePrivateKey(msg);
+            const publicKey = recoverPublicKey(privateKey);
+            key.privateKey = privateKey;
+            key.publicKey = publicKey;
+            key.roundMessage = message;
+
+            setSigningKeys((prevKeys) => {
+              const updateKeys = { ...prevKeys, [address]: key };
+              localStorage.setItem("signingKeys", JSON.stringify(updateKeys));
+
+              return updateKeys;
+            });
+          })
+          .catch((err) => {
+            const error = new Error("Sign failed.");
+            addError(error);
+          });
+      }
+    }
+  }, [
+    signer,
+    address,
+    signingKeys,
+    addError,
+    previousAddress,
+    previousSigner,
+    roundId,
+  ]);
+
+  return {
+    signingKeys,
+    setSigningKeys,
+  };
+}
