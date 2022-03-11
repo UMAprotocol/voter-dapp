4eab37b5b52e26d5b58a0ae302c4f40bcc38e99f
david <david@umaproject.org>
2022-03-11 13:32:49 -0500

    fix: make modal scroll auto
    Signed-off-by: david <david@umaproject.org>

--- src/features/vote/styled/DetailModals.styled.tsx
+++ src/features/vote/styled/DetailModals.styled.tsx
@@ -4,6 +4,7 @@ import tw, { styled } from "twin.macro"; // eslint-disable-line
 export const ModalWrapper = styled.div`
   /* max-width: 700; */
   min-width: 450px;
+  overflow-y: auto;
   height: auto;
   position: relative;
   background-color: #fff;
